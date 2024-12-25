25b43c8b0864d898b2f0bc9fbee7da084e126ead
Tejas Vinay <tejasvinu@gmail.com>
2024-12-25 17:36:45 +0530

    Add Google Auth token to chat message request for authentication

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -1,11 +1,14 @@
 import api from './api';
 
 export const chatService = {
+  
   sendMessage: async (message, history, assistantType) => {
+    const googleAuthToken = localStorage.getItem('googleAuthToken');
     const response = await api.post('/api/google/chat', {
       message,
       history,
-      assistantType
+      assistantType,
+      googleAuthToken
     });
     return response.data;
   }
